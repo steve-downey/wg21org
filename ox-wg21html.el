1ed41124b4ccff12b72d1ce737a0a2b36e2e7ea5
Steve Downey <sdowney@gmail.com>
2024-05-13 09:49:44 -0400

    Add section marks with self-links
    Add section marks to headlines that self-link to the section.

--- ox-wg21html.el
+++ ox-wg21html.el
@@ -95,6 +95,7 @@
 
   :translate-alist '((special-block . my-html-special-block)
                      (inner-template . my-wg21-html-inner-template)
+                     (headline . my-wg21-html-headline)
                      (keyword . my-wg21-html-keyword)
                      (template . my-wg21-html-template))
 
@@ -280,6 +281,80 @@ of contents as a string, or nil if it is empty."
 			                  top-level))
 		            toc
 		            (format "</%s>\n" outer-tag))))))))
+
+;;;; Headline
+
+(defun my-wg21-html-headline (headline contents info)
+  "Transcode a HEADLINE element from Org to HTML.
+CONTENTS holds the contents of the headline.  INFO is a plist
+holding contextual information."
+  (unless (org-element-property :footnote-section-p headline)
+    (let* ((numberedp (org-export-numbered-headline-p headline info))
+           (numbers (org-export-get-headline-number headline info))
+           (level (+ (org-export-get-relative-level headline info)
+                     (1- (plist-get info :html-toplevel-hlevel))))
+           (todo (and (plist-get info :with-todo-keywords)
+                      (let ((todo (org-element-property :todo-keyword headline)))
+                        (and todo (org-export-data todo info)))))
+           (todo-type (and todo (org-element-property :todo-type headline)))
+           (priority (and (plist-get info :with-priority)
+                          (org-element-property :priority headline)))
+           (text (org-export-data (org-element-property :title headline) info))
+           (tags (and (plist-get info :with-tags)
+                      (org-export-get-tags headline info)))
+           (full-text (funcall (plist-get info :html-format-headline-function)
+                               todo todo-type priority text tags info))
+           (contents (or contents ""))
+	       (id (org-html--reference headline info))
+	       (formatted-text
+	        (if (plist-get info :html-self-link-headlines)
+		        (format "<span class=\"content\">%s</span><a class=\"self-link\" href=\"#%s\"></a>" full-text id)
+	          full-text)))
+      (if (org-export-low-level-p headline info)
+          ;; This is a deep sub-tree: export it as a list item.
+          (let* ((html-type (if numberedp "ol" "ul")))
+	        (concat
+	         (and (org-export-first-sibling-p headline info)
+		          (apply #'format "<%s class=\"org-%s\">\n"
+			             (make-list 2 html-type)))
+	         (org-html-format-list-item
+	          contents (if numberedp 'ordered 'unordered)
+	          nil info nil
+	          (concat (org-html--anchor id nil nil info) formatted-text)) "\n"
+	         (and (org-export-last-sibling-p headline info)
+		          (format "</%s>\n" html-type))))
+	    ;; Standard headline.  Export it as a section.
+        (let ((extra-class
+	           (org-element-property :HTML_CONTAINER_CLASS headline))
+	          (headline-class
+	           (org-element-property :HTML_HEADLINE_CLASS headline))
+              (first-content (car (org-element-contents headline))))
+          (format "<%s id=\"%s\" class=\"%s\">%s%s</%s>\n"
+                  (org-html--container headline info)
+                  (format "outline-container-%s" id)
+                  (concat (format "outline-%d" level)
+                          (and extra-class " ")
+                          extra-class)
+                  (format "\n<h%d class=\"heading\" id=\"%s\" %s>%s</h%d>\n"
+                          level
+                          id
+			              (if (not headline-class) ""
+			                (format " class=\"%s\"" headline-class))
+                          (concat
+                           (and numberedp
+                                (format
+                                 "<span class=\"section-number-%d\">%s</span> "
+                                 level
+                                 (concat (mapconcat #'number-to-string numbers ".") ".")))
+                           formatted-text)
+                          level)
+                  ;; When there is no section, pretend there is an
+                  ;; empty one to get the correct <div
+                  ;; class="outline-...> which is needed by
+                  ;; `org-info.js'.
+                  (if (eq (org-element-type first-content) 'section) contents
+                    (concat (org-html-section first-content "" info) contents))
+                  (org-html--container headline info)))))))
 
 (defun my-wg21-html-keyword (keyword _contents info)
   "Transcode a KEYWORD element from Org to HTML.
